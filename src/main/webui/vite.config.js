7ad494204ce85231ca9c6bf653297b8620046b6b
yuokada <yuokada@users.noreply.github.com>
2025-05-04 03:48:42 +0900

    Save

--- src/main/webui/vite.config.js
+++ src/main/webui/vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// https://docs.quarkiverse.io/quarkus-quinoa/dev/web-frameworks.html#vite-config
 export default defineConfig({
+  base: '',
   plugins: [react()],
+  // plugins: [react(), viteTsconfigPaths()],
+  // server: {
+  //   port: 8080,
+  //   open: true
+  // },
 })
